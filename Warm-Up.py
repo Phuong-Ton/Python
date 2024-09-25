e1e4daab1da42cda846e8f603475900321f620a1
Phuong-Ton <phuongtonthat@gmail.com>
2024-09-25 15:21:05 -0700

    Add

---
+++ Warm-Up.py
@@ -0,0 +1,137 @@
+print("Hello \n Python");print("Hello Python 2");print("Hello Python 3");
+print("haha","joho","hehe","combine all of parameters/ agument", end="@")
+print("Join string", "bcs", sep=":")
+
+print("Octal number 	0o123 - 8^2 + 8^1 + 8^0 = 64*1 + 8*2 + 1*3= 64 + 16 + 1 = 81")
+print("Hexadecimal 0x123 = 16^2 * 1 +16^1 * 2 + 16^0*3")
+'''
+Operator:
+exponential: ** double asterisks / superscript
+2**3 = 2^3
+multiplication: * asterisks
+division /
+flour division: //
+modulo %
+
+lowest priority
+subtraction: -
+addition: +
+
+shortcut operator +=
+concaternate the string / combine text : +
+hash character
+
+in string can you * aterisk to repeat n time ha * 3 -> hahaha
+
+ha * -1 -> ""
+ha * 0 -> ""
+'''
+print("print(2**3)")
+print(2**3)
+
+print("print(6/4)")
+print(6/4)
+print("print(6//4), down")
+print(6	//4)
+
+# using hash character to comment
+
+'''
+input() value is string
+wrap the value in int function/ we need to cast the value first.
+
+'''
+
+
+
+'''
+Comparation operater
+== equal to
+<= less than or equal
+>= greater than or equal
+!= not equal to 
+< less than
+> greater than
+
+
+left hand parentless
+
+'''
+
+'''
+if condition:
+  print(1)
+elif condition2:
+else:
+  print(2)
+
+'''
+
+'''
+Loop
+
+while condition:
+  Loop statement
+  
+for i in range(5):
+
+0 1 2 3 4
+
+for i in range(2,5):
+2 3 4 
+
+
+
+using break to end the loop
+We want to iterate over the values from 0 to 10,
+However, we want to skip all the values that are even (1%2)!= 0)
+                                                 odd (1%2 == 0)
+
+'''
+
+'''
+bit operator
+and
+or
+not
+a = true
+if not a -> if a == false:
+
+
+bitwise
+& conjunction
+| disjunction
+~ negation
+^ exclusive
+ bit of data
+ 
+15&22
+ 00001111 
+ 00010110
+&00000110
+
+ 00001111 
+ 00010110
+|00111111
+
+ 00001111 
+ 00010110
+^00011001
+ 
+ 00010110
+~11101001
+
+bit shifting
+>> <<
+
+00010110
+00001011 >> 1
+00010110 << 1
+'''
+
+print(bin(16))
+
+# [128][64][32][16][8][4][2][1]
+# 16-> 0b10000
+
+
